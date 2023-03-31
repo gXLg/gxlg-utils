ac6e5af7da75c1e215179a77e086b25f88575c5d
gXLg <natgaev@gmail.com>
2023-03-31 14:19:31 +0200

    upd

--- index.js
+++ index.js
@@ -1,45 +1,5 @@
-// https://stackoverflow.com/a/61548724/13008170
-function jsonCompare(arg1, arg2){
-  if(
-    Object.prototype.toString.call(arg1) ===
-    Object.prototype.toString.call(arg2)
-  ){
-    if(
-      Object.prototype.toString.call(arg1) ===
-      "[object Object]" ||
-      Object.prototype.toString.call(arg1) ===
-      "[object Array]"
-    ){
-      if(
-        Object.keys(arg1).length !==
-        Object.keys(arg2).length
-      ){
-        return false;
-      }
-      return (Object.keys(arg1).every(
-        key => jsonCompare(arg1[key], arg2[key])
-      ));
-    }
-    return (arg1 === arg2);
-  }
-  return false;
-}
+const json = require("./collection/json.js");
+const TimePromise = require("./collection/timepromise.js");
+const mimes = require("./collection/mimes.json");
 
-function jsonCopy(obj){
-  if(obj instanceof Object){
-    if(obj instanceof Array){
-      const n = [];
-      for(const o of obj){
-        n.push(jsonCopy(o));
-      }
-      return n;
-    }
-    const n = { };
-    for(const key in obj)
-      n[key] = jsonCopy(obj[key]);
-    return n;
-  }
-  return obj;
-}
-
-module.exports = { jsonCompare, jsonCopy };
+module.exports = { json, TimePromise, mimes };

4fd8192b0cb0d28d1b24ada3ec9bb69d9119f7a4
gXLg <natgaev@gmail.com>
2025-01-10 12:59:47 +0100

    custom errors for optparser; verification param

--- collection/optparser.js
+++ collection/optparser.js
@@ -1,29 +1,33 @@
-module.exports = (args = []) => {
+module.exports = (args = [], error = Error) => {
   if (args == null || args.constructor != [].constructor) {
     throw new Error("Parser: Argument 'args' has a wrong type! Expected one of: Array");
   }
+  if (error == null || !(error.prototype instanceof Error || error === Error)) {
+    throw new Error("Parser: Argument 'customError' has a wrong type! Expected one of: class Error");
+  }
+
   for (let i = 0; i < args.length; i++) {
     const arg = args[i];
     if (arg == null || arg.constructor != {}.constructor) {
-      throw new Error("Parser: Argument field 'args[" + i + "]' has a wrong type! Expected one of: Object");
+      throw new error("Parser: Argument field 'args[" + i + "]' has a wrong type! Expected one of: Object");
     }
-    for (const [param, r, t] of [["name", true, ""], ["required", false, false], ["types", true, []]]) {
+    for (const [param, r, t] of [["name", true, ""], ["required", false, false], ["types", true, []], ["verify", false, () => true]]) {
       if (arg[param] == null && !r) { arg[param] = t; }
       if (!(param in arg)) {
-        throw new Error("Parser: Argument field 'args[" + i + "]." + param + "' is required");
+        throw new error("Parser: Argument field 'args[" + i + "]." + param + "' is required");
       }
       if (arg[param] == null || arg[param].constructor != t.constructor) {
-        throw new Error("Parser: Argument field 'args[" + i + "]." + param + "' has a wrong type! Expected one of: " + t.constructor.name);
+        throw new error("Parser: Argument field 'args[" + i + "]." + param + "' has a wrong type! Expected one of: " + t.constructor.name);
       }
     }
   }
 
   return (opt = {}) => {
     if (opt == null || opt.constructor != {}.constructor) {
-      throw new Error("Argument 'options' has a wrong type! Expected one of: Object");
+      throw new error("Argument 'options' has a wrong type! Expected one of: Object");
     }
     const options = {};
-    for (const { name, required, types } of args) {
+    for (const { name, required, types, verify } of args) {
       const value = opt[name];
       let valid = value == null;
       if (!valid) {
@@ -35,35 +39,19 @@ module.exports = (args = []) => {
         }
       }
       if (!valid) {
-        throw new Error(
+        throw new error(
           "Argument '" + name + "' has a wrong type! Expected one of: " +
           [...new Set(types.map(d => d.constructor.name))].join(", ")
         );
       }
       if (required && value == null) {
-        throw new Error("Non-null argument '" + name + "' is required");
+        throw new error("Non-null argument '" + name + "' is required");
+      }
+      if (!verify(value)) {
+        throw new error("Argument '" + name + "' failed verification");
       }
       options[name] = value ?? types[0];
     }
     return options;
   };
 };
-
-
-
-/*
-
-  const args = [
-    ["cookie", false, "token"],
-    ["path", true, ""],
-    ["secret", true, ""],
-    ["username", false, 24],
-    ["secure", false, true],
-    ["verify", false, false]
-  ];
-
-  const options = { };
-
-
-
-*/
